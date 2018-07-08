0135eadc425d9b35da660d03c325a59ccc704c32
Luiz França <luizdaniel.r.f@gmail.com>
2018-07-08 17:54:52 -0300

    Exercise 2: Practice writing functions

---
+++ LearnKotlin/src/HelloWorld.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+
+    var lukeReturns = "I'm cool"
+
+    fun faceDarthVader(say: String) {
+        println("And Luke said: $say")
+    }
+
+    faceDarthVader(lukeReturns)
+}
